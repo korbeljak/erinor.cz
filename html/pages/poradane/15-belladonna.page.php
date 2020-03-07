653326caa3994de577e02f8237de817207ee75c1
Jakub Korbel <korbel.jak@gmail.com>
2020-03-07 15:22:52 +0100

    Belladonna

---
+++ html/pages/poradane/15-belladonna.page.php
@@ -0,0 +1,26 @@
+<?php /*
+title = "Stránky LARPu Erinor (pořádané skupinou Pilirion) &ndash; XV. Belladonna";
+keywords = "larp, erinor, fantasy, dřevárny, roleplay, svět, belladonna, bella, donna";
+description = "Stránky LARPu Erinor - Pořádané: XV. Belladonna";
+
+Sem lze napsat libovolný komentář, nebude zobrazen. Zobrazí se pouze text pod čarou.
+----------
+*/ ?>
+<h1>Erinor XV. &ndash; Belladonna</h1>
+
+<p>Ve světě Erinoru se začíná odvíjet další kapitola. Kapitola z minulosti, kterou
+všichni moc dobře znají. Z minulosti, která zahýbala světem. Nad
+Eirem se stahují mračna.
+</p>
+
+<p>
+Již nějakou dobu jsou mnohé a početné kulty Erinoru císařství trnem v oku. Jejich
+moc roste, možná až příliš. První konflikty státní a církevní moci vyvolaly 
+napětí. Něco se bude dít. Proto je načase svolat konkláve 
+jednotlivých církví a kultů a poradit se, co dál.
+</p>
+
+<p>
+Které místo bude zvoleno? Ovlivní konkláve chod života běžných lidí? Jak se
+zachová císařství?
+</p>
